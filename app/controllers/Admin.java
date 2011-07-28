77737f785374eafe36d8e48b3c79a6eff01f9423
David Schlyter <dschlyter@gmail.com>
2011-07-28 02:02:45 +0200

    Login for CRUD main admin page now as well

---
+++ app/controllers/Admin.java
@@ -0,0 +1,11 @@
+package controllers;
+ 
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+ 
+@With(Secure.class)
+public class Admin extends CRUD {
+	// TODO fancier admin page maybe
+}
