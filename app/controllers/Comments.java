d8f300ea61e5abd72f5139db74413e5d1d7953b2
David Schlyter <dschlyter@gmail.com>
2011-08-19 22:26:05 +0200

    Added: Realtime comment updates        Comment on load via ajax (removed code duplication) Todo:  Break out to separate file and framework-ize

--- app/controllers/Comments.java
+++ app/controllers/Comments.java
@@ -1,10 +1,14 @@
 package controllers;
 
-import ext.json.serializers.CommentJsonSerializer;
 import models.Comment;
 import models.Receipt;
 import play.*;
 import play.mvc.*;
+import helpers.serializers.CommentJsonSerializer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * For CRUD-interface
@@ -20,10 +24,23 @@ public class Comments extends CRUD
 	 * @param id receipt to add the comment to
 	 * @param content content of comment
 	 */
-	public static void post(Long id, String content)
+	public static void post(Long id, String content, Long lastPost)
 	{
 		Receipt receipt = Receipt.findById(id);
-		Comment comment = new Comment(receipt, Security.connectedUser(), content).save();
-		renderJSON(comment, new CommentJsonSerializer());
+		new Comment(receipt, Security.connectedUser(), content).save();
+		getCommentsCommon(id, lastPost);
+	}
+	
+	public static void get(Long id, Long lastPost) {
+		getCommentsCommon(id, lastPost);
+	}
+	
+	static void getCommentsCommon(Long id, Long lastPost) {
+		if(lastPost < 0) lastPost = 0L; // Avoid exception
+		Date date = new Date(lastPost);
+
+		// TODO add enterprise generalized code with generics and interfaces
+		List<Comment> comments = Comment.find("receipt.id = ? AND date > ? ORDER BY date asc", id, date).fetch();
+		renderJSON(comments, new CommentJsonSerializer());
 	}
 }
