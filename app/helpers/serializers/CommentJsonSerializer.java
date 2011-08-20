21a400504ab2ecc4a73959aa9bc3b81b7eca922d
Peksa <git@peksa.se>
2011-08-20 21:44:34 +0200

    added facebook-style timestamps "8 minutes ago"

--- app/helpers/serializers/CommentJsonSerializer.java
+++ app/helpers/serializers/CommentJsonSerializer.java
@@ -1,5 +1,6 @@
 package helpers.serializers;
 
+import com.ocpsoft.pretty.time.PrettyTime;
 
 import java.lang.reflect.Type;
 import java.text.DateFormat;
@@ -19,12 +20,15 @@ import play.templates.JavaExtensions;
 	@Override
 	public JsonElement serialize(Comment comment, Type t, JsonSerializationContext jsc)
 	{
+		// Get pretty time
+		PrettyTime p = new PrettyTime();
+		
 		JsonObject obj = new JsonObject();
 		obj.addProperty("id", comment.id);
 		obj.addProperty("own", (comment.poster == comment.receipt.owner));
 		obj.addProperty("poster", comment.poster.fullname);
 		obj.addProperty("content", comment.content);
-		obj.addProperty("date", comment.getFormattedDate("yyyy-MM-dd"));
+		obj.addProperty("date", p.format(comment.date));
 		obj.addProperty("timestamp", comment.date.getTime());
 		return obj;
 	}
