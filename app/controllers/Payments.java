3c23e71c7e574fe8ed52e1ededf0ac98c16806ad
David Schlyter <dschlyter@gmail.com>
2011-09-06 23:50:23 +0200

    Very basic UI for listing payments

--- app/controllers/Payments.java
+++ app/controllers/Payments.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import models.Payment;
@@ -20,9 +21,15 @@ import play.mvc.*;
 public class Payments extends CRUD
 {
 	// Helper for index
-	static void increment(HashMap<Long,Integer> map, Long key, Integer value) {
+	static void increment(HashMap<User,Integer> map, User key, Integer value) {
 		if(map.containsKey(key)) map.put(key, map.get(key) + value);
 		else map.put(key, value);
+	}
+	
+	public static void index() {
+		User connected = Security.connectedUser();
+		if (connected != null)
+			show(connected.id);
 	}
 	
 	/**
@@ -30,44 +37,50 @@ public class Payments extends CRUD
 	 * This matches incoming and outgoing debts over time and settles any difference
 	 * @param userId
 	 */
-	public static void index(Long userId) {
+	public static void show(Long userId) {
 		validation.required(userId);
 		if(!validate(userId)) return;
-		
 		
 		//TODO(dschlyter) This operation is pretty expensive, but premature optimization is the root of all evil etc
 		// Opt Idea 1: Cache result and invalidate on new receipt with user involved
 		// Opt Idea 2: Cache result and date and only use new data to re-calculate (harder)
 		
 		User user = User.findById(userId);
-		HashMap<Long, Integer> debt = new HashMap<Long, Integer>();
+		HashMap<User, Integer> debt = new HashMap<User, Integer>();
 		// Track debt that is linked to fresh receipts
-		HashMap<Long, Integer> freshDebt = new HashMap<Long, Integer>(); 
+		HashMap<User, Integer> freshDebt = new HashMap<User, Integer>();
+		HashMap<User, HashSet<Receipt>> freshReceipts = new HashMap<User, HashSet<Receipt>>();
 		
 		// Sum all receipts where you owe money, subtract sum of all receipts owned
 		for(Receipt r : user.incomingReceipts) {
 			int total = r.getTotal(user);
-			increment(debt, r.owner.id, total);
-			if(r.hasPayment(user)) increment(freshDebt, r.owner.id, total);
+			increment(debt, r.owner, total);
+			if(!r.hasPayment(user)) {
+				increment(freshDebt, r.owner, total);
+				//freshReceipts.get(r.owner).add(r);
+			}
 		}
 		for(Receipt r : user.receipts) {
 			for(User u : r.members) {
 				int total = r.getTotal(u);
-				increment(debt, u.id, -total);
-				if(r.hasPayment(u)) increment(freshDebt, u.id, -total);
+				increment(debt, u, -total);
+				if(!r.hasPayment(u)) { 
+					increment(freshDebt, u, -total);
+					//freshReceipts.get(r.owner).add(r);
+				}
 			}
 		}
 		
 		// Sum all payments received, subtract sum of all payments made
 		for(Payment p : user.incomingPayments) {
-			increment(debt, p.payer.id, p.amount);
+			increment(debt, p.payer, p.amount);
 		}
 		for(Payment p : user.payments) {
-			increment(debt, p.receiver.id, -p.amount);
+			increment(debt, p.receiver, -p.amount);
 		}
 		
 		// TODO maybe remove 0 summed
-		// TODO render(debt, freshDebt)
+		render(debt, freshDebt, freshReceipts);
 	}
 
 	
