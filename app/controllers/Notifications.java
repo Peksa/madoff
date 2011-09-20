52217962b6fe72656bbbc6b8d7c99867acc5019b
Peksa <git@peksa.se>
2011-09-20 22:48:32 +0200

    Proof of concept notification system using long polling.

---
+++ app/controllers/Notifications.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.Logger;
+import play.libs.F.EventStream;
+import play.mvc.*;
+
+@With(Secure.class)
+public class Notifications extends CRUD
+{
+	public static EventStream liveStream = new EventStream();
+	
+    public static void stream()
+    {
+    	String notification = await(liveStream.nextEvent());
+    	renderText(notification);
+    }
+    
+    /**
+     * Temporary for adding broadcast-notifications.
+     * @param text notification text to appear in red box of all clients.
+     */
+    public static void add(String text)
+    {
+    	liveStream.publish(text);
+    }
+
+}
